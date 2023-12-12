de300d6b1fd69e89571ddc7ad6e1476c1d319f07
Ulrich Leodolter <ulrich.leodolter@obvsg.at>
2023-12-12 14:00:26 +0100

    new build

--- js/primoStudioPlumx.js
+++ js/primoStudioPlumx.js
@@ -5,11 +5,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
-
 var _plumx = _interopRequireDefault(require("./plumx.controller"));
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
 /*
  * plumx.component.js
  */
@@ -32,20 +29,15 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-
 /*
  * plumx.controller.js
  */
 var PrimoStudioPlumxController = /*#__PURE__*/function () {
   function PrimoStudioPlumxController(angularLoad, studioConfig, $http, $scope, $element, $timeout, $window) {
     _classCallCheck(this, PrimoStudioPlumxController);
-
     this.angularLoad = angularLoad;
     this.studioConfig = studioConfig;
     this.$http = $http;
@@ -54,7 +46,6 @@ var PrimoStudioPlumxController = /*#__PURE__*/function () {
     this.$timeout = $timeout;
     this.$window = $window;
   }
-
   _createClass(PrimoStudioPlumxController, [{
     key: "getConfigApiKey",
     value: function getConfigApiKey() {
@@ -80,12 +71,11 @@ var PrimoStudioPlumxController = /*#__PURE__*/function () {
     value: function $onInit() {
       var vm = this;
       vm.embed_js = '';
-      vm.plumx_js = 'https://d39af2mgp1pqhg.cloudfront.net/widget-';
+      vm.plumx_js = 'https://cdn.plu.mx/widget-';
       vm.plumx_cssid = 'plx-css-';
       vm.plumx_url = '';
       vm.plumx_popup = '';
       vm.plumx_badge = '';
-
       switch (vm.getConfigWidgetType()) {
         default:
         case 'print':
@@ -94,19 +84,16 @@ var PrimoStudioPlumxController = /*#__PURE__*/function () {
           vm.plumx_js += 'popup.js';
           vm.plumx_cssid += 'popup';
           break;
-
         case 'details':
           vm.plumx_class = 'plumx-details';
           vm.plumx_js += 'details.js';
           vm.plumx_cssid += 'details';
           break;
-
         case 'summary':
           vm.plumx_class = 'plumx-summary';
           vm.plumx_js += 'summary.js';
           vm.plumx_cssid += 'summary';
           break;
-
         case 'badge':
           vm.plumx_class = 'plumx-plum-print-popup';
           vm.plumx_popup = 'right';
@@ -115,25 +102,21 @@ var PrimoStudioPlumxController = /*#__PURE__*/function () {
           vm.plumx_cssid += 'popup';
           break;
       }
-
       switch (vm.getConfigWidgetTheme()) {
         default:
         case 'default':
           break;
-
         case 'bigben':
           vm.plumx_class += ' plumx-bigben-theme';
           break;
-
         case 'liberty':
           vm.plumx_class += ' plumx-liberty-theme';
           break;
-      } // the prm-full-view container, our parent is prm-full-view-after
+      }
 
-
+      // the prm-full-view container, our parent is prm-full-view-after
       vm.parentElement = vm.$element.parent().parent()[0];
       vm.api = 'doi';
-
       try {
         vm.doi = vm.parentCtrl.item.pnx.addata.doi[0] || '';
       } catch (e) {
@@ -146,11 +129,10 @@ var PrimoStudioPlumxController = /*#__PURE__*/function () {
           return;
         }
       }
-
       if (vm.doi) {
         vm.$timeout(function () {
           vm.plumx_url = 'https://plu.mx/plum/a/?' + vm.api + '=' + vm.doi;
-          vm.$http.head('https://cors-anywhere.herokuapp.com/' + vm.plumx_url).then(function (_res) {
+          vm.$http.head(vm.plumx_url).then(function (_res) {
             // Get the PlumX script
             vm.embed_js = vm.plumx_js + '?' + Date.now();
             vm.angularLoad.loadScript(vm.embed_js).then(function () {
@@ -168,20 +150,18 @@ var PrimoStudioPlumxController = /*#__PURE__*/function () {
             console.log('plumx api failed: ' + res);
           });
         }, 3000);
-      } // move the plumx widget into the new PlumX service section
+      }
 
-
+      // move the plumx widget into the new PlumX service section
       var unbindWatcher = vm.$scope.$watch(function () {
         return vm.parentElement.querySelector('h4[translate="brief.results.tabs.PlumX"]');
       }, function (newVal, _oldVal) {
         if (newVal) {
           // Get the section body associated with the value we're watching
           var sectionBody = newVal.parentElement.parentElement.parentElement.parentElement.children[1];
-
           if (sectionBody && sectionBody.appendChild) {
             sectionBody.appendChild(vm.$element[0]);
           }
-
           unbindWatcher();
         }
       });
@@ -191,25 +171,20 @@ var PrimoStudioPlumxController = /*#__PURE__*/function () {
     value: function $onDestroy() {
       var vm = this;
       var el = null;
-
       if (vm.$window.__plumX) {
         delete vm.$window.__plumX;
-      } // remove css/js
+      }
+
+      // remove css/js
       // http://www.javascriptkit.com/javatutors/loadjavascriptcss2.shtml
-
-
       el = document.body.querySelector('[src="' + vm.embed_js + '"]');
-
       if (el) {
         el.remove();
       }
-
       el = document.head.querySelector('link[id="' + vm.plumx_cssid + '"]');
-
       if (el) {
         el.remove();
       }
-
       document.head.querySelectorAll('script').forEach(function (script) {
         if (script.src.endsWith('jquery/1.10.2/jquery.min.js') || script.src.endsWith('extjs/xss.js')) {
           script.parentNode.removeChild(script);
@@ -217,10 +192,8 @@ var PrimoStudioPlumxController = /*#__PURE__*/function () {
       });
     }
   }]);
-
   return PrimoStudioPlumxController;
 }();
-
 PrimoStudioPlumxController.$inject = ['angularLoad', 'primoStudioPlumxStudioConfig', '$http', '$scope', '$element', '$timeout', '$window'];
 var _default = PrimoStudioPlumxController;
 exports["default"] = _default;
@@ -232,25 +205,26 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.PrimoStudioPlumxModule = void 0;
-
 var _plumx = _interopRequireDefault(require("./plumx.component"));
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
 /**
  * plumx.module.js
  */
-var PrimoStudioPlumxModule = angular.module('primoStudioPlumx', []).component(_plumx["default"].selector, _plumx["default"]).name;
+
+var PrimoStudioPlumxModule = angular.module('primoStudioPlumx', []).component(_plumx["default"].selector, _plumx["default"]).config(['$sceDelegateProvider', function ($sceDelegateProvider) {
+  var whitelist = $sceDelegateProvider.trustedResourceUrlList();
+  $sceDelegateProvider.trustedResourceUrlList(whitelist.concat(['https:/plu.mx/**']));
+}]).name;
 exports.PrimoStudioPlumxModule = PrimoStudioPlumxModule;
 
 },{"./plumx.component":1}],4:[function(require,module,exports){
 "use strict";
 
 var _plumx = require("./js/plumx.module");
-
 /**
  * main.js
  */
+
 app.requires.push(_plumx.PrimoStudioPlumxModule);
 
 },{"./js/plumx.module":3}]},{},[4]);
